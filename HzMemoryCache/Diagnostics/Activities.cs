8ab0296e0ab42e18629f768a8aad968c790de5de
Joel Nygren <joel.nygren@norce.io>
2024-11-19 15:19:53 +0100

    tidy

--- HzMemoryCache/Diagnostics/Activities.cs
+++ HzMemoryCache/Diagnostics/Activities.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace HzCache.Diagnostics
@@ -10,7 +9,7 @@ namespace HzCache.Diagnostics
 
         public static class Names
         {
-            public const string Set = " set from cache";
+            public const string Set = "set from cache";
             public const string Get = "get from cache";
             public const string GetOrSet = "get or set from cache";
             public const string GetOrSetBatch = "get or set batch from cache";
@@ -18,22 +17,21 @@ namespace HzCache.Diagnostics
             public const string RemoveByPattern = "remove by pattern";
             public const string RemoveItem = "remove item";
             public const string Clear = "clear";
-            
+
             public const string NotifyItemChange = "notify item change";
             public const string Subscribe = "subscribe";
             public const string ValueChanged = "value changed";
             public const string EvictExpired = "evict expired";
             public const string GetStatistics = "get statistics";
-            
         }
+
         public static class Project
         {
             public const string HzMemoryCache = "HzMemoryCache";
             public const string RedisBackedHzCache = "RedisBackedHzCache";
-            
         }
 
-        private static IEnumerable<KeyValuePair<string, object>> GetCommonTags(string? key,string project,bool async, string? pattern, bool? sendNotification)
+        private static IEnumerable<KeyValuePair<string, object>> GetCommonTags(string? key, string project, bool async, string? pattern, bool? sendNotification)
         {
             var res = new List<KeyValuePair<string, object?>>
             {
@@ -47,7 +45,7 @@ namespace HzCache.Diagnostics
             return res;
         }
 
-        public static Activity? StartActivityWithCommonTags(this ActivitySource source, string activityName,string project,bool async=false, string? key = null, string? pattern = null, bool? sendNotification = null)
+        public static Activity? StartActivityWithCommonTags(this ActivitySource source, string activityName, string project, bool async = false, string? key = null, string? pattern = null, bool? sendNotification = null)
         {
             if (source.HasListeners() == false || !HzCacheTracesInstrumentationOptions.Instance.IsActive(activityName))
                 return null;
@@ -58,8 +56,5 @@ namespace HzCache.Diagnostics
                 name: activityName
             );
         }
-
-
-   
     }
 }
